c816ee144b9d41978d7a016f375a55850349041c
olgaodc <olgaodincova@gmail.com>
2023-07-04 02:34:49 +0300

    question model changed

--- api/models/question.js
+++ api/models/question.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const questionSchema = mongoose.Schema({
-    text: {type: String, required: true, min: 5},
-    description: {type: String, required: true, min: 10},
+    text: {type: String, required: true, min: 10},
+    description: {type: String, required: true, min: 20},
     id: {type: String, required: true, min: 3},
     creationDate: {type: Date, required: true},
     answersIds: {type: Array, required: false},
